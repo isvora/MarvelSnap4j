f5b3a28b10998b8de503f002f19dae49683c7bcd
isvora <isvoranandrei25@gmail.com>
2022-11-25 10:18:17 +0200

    Add more collection levels

--- src/main/java/org/isvora/snap/type/CollectionLevel.java
+++ src/main/java/org/isvora/snap/type/CollectionLevel.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CollectionLevel {
 
+    STARTER_CARD("Starter Card"),
+    SEASON_01("Season 01"),
+    RECRUIT_SEASON("Recruit Season"),
     BASIC("Collection Level 1-14"),
     POOL_1("Collection Level 18-214 (Pool 1)"),
     POOL_2("Collection Level 222-450 (Pool 2)"),
